4b566e3fd1962b891a7b23aa5ef41f8a20f1bfaa
Madhav-Robocon <madhavbhanushalirobocon@gmail.com>
2025-05-06 19:50:56 +0530

    Update .gitignore

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://21b04e84b4f78754a068e59e62da3c89@o4509248541360128.ingest.de.sentry.io/4509248559579216",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
